7c0f69f738d97b00517aa54dc1e172a80d2c4954
ZulfikarSatria <abim23957@gmail.com>
2026-04-07 23:57:07 +0700

    Adding program to Hand and PokerHandChecker

---
+++ Chain of Responsibility/Chain of Responsibility/Hand.h
@@ -0,0 +1,4 @@
+#pragma once
+struct Hand {
+	int value = 0;
+};
--- Chain of Responsibility/Chain of Responsibility/PokerHandChecker.h
+++ Chain of Responsibility/Chain of Responsibility/PokerHandChecker.h
@@ -1,5 +1,18 @@
 #pragma once
-class PokerHandChecker
-{
+#include "Hand.h"
+enum class HandRank {
+	HIGH_CARD,
+	PAIR,
+	TWO_PAIR,
+	THREE_OF_A_KIND,
+	STRAIGHT,
+	FLUSH,
+	FULL_HOUSE
 };
-
+class PokerHandChecker {
+public:
+	virtual HandRank check(const Hand& hand) = 0;
+	void setNext(PokerHandChecker* next);
+protected:
+	PokerHandChecker* nextChecker = nullptr;
+};
