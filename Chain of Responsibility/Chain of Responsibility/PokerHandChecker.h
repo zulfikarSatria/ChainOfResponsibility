9d7daafb0c3869d7ad34f571692007ba8bd1bfb4
ZulfikarSatria <abim23957@gmail.com>
2026-04-08 00:04:18 +0700

    Add more poker hand to PokerHandChecker
    Ada yang belum tertulis tadi, kayak hidden poker cardnya

--- Chain of Responsibility/Chain of Responsibility/PokerHandChecker.h
+++ Chain of Responsibility/Chain of Responsibility/PokerHandChecker.h
@@ -7,7 +7,13 @@ enum class HandRank {
 	THREE_OF_A_KIND,
 	STRAIGHT,
 	FLUSH,
-	FULL_HOUSE
+	FULL_HOUSE,
+	FOUR_OF_A_KIND,
+	STRAIGHT_FLUSH,
+	ROYAL_FLUSH,
+	FIVE_OF_A_KIND,
+	FLUSH_HOUSE,
+	FLUSH_FIVE
 };
 class PokerHandChecker {
 public:
